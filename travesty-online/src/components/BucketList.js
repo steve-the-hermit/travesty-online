e0b388d28fb655a605861fb80f0800fa6a7adce5
Krystal Mark <krystal.mark@student.moringaschool.com>
2023-08-02 14:05:11 +0300

    bucketlist

---
+++ travesty-online/src/components/BucketList.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchBucketList } from './api';
+
+const BucketList = () => {
+  const [bucketList, setBucketList] = useState([]);
+
+  useEffect(() => {
+    fetchBucketList()
+      .then((data) => {
+        setBucketList(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching bucket list: ', error);
+      });
+  }, []);
+
+  return (
+    <div>
+    <h2>My Bucket List</h2>
+    {bucketList.length === 0 ? (
+      <p>No destinations in your bucket list.</p>
+    ) : (
+      <ul>
+        {bucketList.map((destination) => (
+          <li key={destination.id}>
+            <Link to={`/destination/${destination.id}`}>
+              <h3>{destination.name}</h3>
+              <img src={destination.image} alt={destination.name} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+      
+    )}
+    <Link to="/add">Add Destination</Link>
+  </div>
+  );
+};
+
+export default BucketList;
