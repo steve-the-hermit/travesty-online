cfeca46abbe44c8f9129382d7ce4df25180338ec
Krystal Mark <krystal.mark@student.moringaschool.com>
2023-08-02 13:45:35 +0300

    api

---
+++ travesty-online/src/components/api.js
@@ -0,0 +1,45 @@
+const BASE_URL = 'http://localhost:3002'; 
+
+export const getDestinations = () => {
+  return fetch(`${BASE_URL}/destinations`)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => {
+      console.error('Error fetching destinations:', error);
+      return [];
+    });
+};
+
+export const addDestination = (newDestination) => {
+  return fetch(`${BASE_URL}/destinations`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newDestination),
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error adding destination:', error);
+    });
+};
+
+export const addDestinationToBucketList = async (destination) => {
+  try {
+    const response = await fetch(`${BASE_URL}/bucketlist`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(destination),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to add destination to Bucket List');
+    }
+
+    
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
